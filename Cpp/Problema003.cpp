d1462c130bb6785b11a4eef2e9f76d991b905f1c
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-10-10 02:10:25 -0500

    Realizado problema 3 en C++

---
+++ Cpp/Problema003.cpp
@@ -0,0 +1,36 @@
+/* ========== Project Euler. Problem #3 ==========
+*
+* The prime factors of 13195 are 5, 7, 13 and 29.
+*
+* What is the largest prime factor of the number 600851475143 ?
+*
+* -----------------------------------------------*/
+
+#include <iostream>
+
+int main()
+{
+    // El número que hay que evaluar
+    long n, m;
+    std::cout << "Escriba un número: ";
+    std::cin >> n;
+
+    // Variable que contiene el primo actual y el mayor primo
+    int p = 2, pmax;
+
+    // Copia del número
+    m = n;
+    // Se divide al número con el actual número primo, si no puede dividirse,
+    // entonces avanza al siguiente primo
+    while(m>1){
+        if(m%p==0){
+            pmax = p;
+            m /= p;
+        } else
+            p += 1;
+    }
+
+    std::cout << "El mayor factor primo de " << n << " es " << pmax << std::endl;
+
+    return 0;
+}
