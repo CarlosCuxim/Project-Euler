81ca0d751273f1d74dc3ebb173543f1ba4fce20e
Carlos Cuxim <carlos.cuxim@gmail.com>
2022-10-16 20:12:52 -0500

    Realizado problema 10 en C++

---
+++ Cpp/Problema010.cpp
@@ -0,0 +1,38 @@
+/* ========== Project Euler. Problem #10 =========
+*
+* The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+*
+* Find the sum of all the primes below two million.
+*
+* -----------------------------------------------*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int);
+
+int main()
+{
+    int n;
+    cout << "Escriba el límite: ";
+    cin >> n;
+
+    long S=0;
+    for(int i=2; i<n; ++i){
+        if(isPrime(i))
+            S += i;
+    }
+
+    cout << "La suma de los primos menores a " << n << " es " << S << endl;
+
+    return 0;
+}
+
+bool isPrime(int n){
+    for(int i=2; i<=sqrt(n); ++i){
+        if(!(n%i))
+            return false;
+    }
+    return true;
+}
