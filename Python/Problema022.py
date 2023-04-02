d5fd8e7c0dbdfb663ea2d5129b6b5213ace786fe
Carlos Cuxim <carlos.cuxim@gmail.com>
2023-04-02 12:13:22 -0600

    Realizado problema 22 en Python

---
+++ Python/Problema022.py
@@ -0,0 +1,39 @@
+# ================================== Project Euler. Problem #22 ====================================
+
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over
+# five-thousand first names, begin by sorting it into alphabetical order. Then working out the
+# alphabetical value for each name, multiply this value by its alphabetical position in the list
+#     to obtain a name score.
+
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 +
+# 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+# What is the total of all the name scores in the file?
+
+# --------------------------------------------------------------------------------------------------
+
+def GetList(dir):
+    with open(dir) as file:
+        L = eval("[" + file.read() + "]")
+    L.sort()
+
+    return L
+
+def LetterPosition(s):
+    return ord(s) - ord("A") + 1
+
+def GetScore(name, pos=1):
+    S = 0
+    for s in name:
+        S += LetterPosition(s)
+    return S*pos
+
+def GetSumListScore(L):
+    S = 0
+    for i in range(len(L)):
+        S += GetScore(L[i], i+1)
+    return S
+
+L = GetList("Python/resources/p022_names.txt")
+
+print(f"El total de todas las puntuaciones de nombres es {GetSumListScore(L)}")
